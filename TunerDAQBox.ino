9d9ddcac5fc8bf0ed260db90301b2b8e4992acb8
Neko Prog <nekoprog@gmail.com>
2017-03-29 21:10:02 +0800

    Add files via upload
    First commit

---
+++ TunerDAQBox.ino
@@ -0,0 +1,26 @@
+int wbLo = 10; //afr user input
+int wbHi = 20; //afr user input
+int wbIn = A0;
+int revIn = A1;
+int spdIn = A2;
+int wbSensor = 0;
+unsigned long revSensor = 0; //counts per second
+unsigned long spdSensor = 0; //counts per second
+float afr = 0.0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  wbSensor = analogRead(wbIn);
+  revSensor = pulseIn(revIn,HIGH); //todo
+  spdSensor = pulseIn(spdIn,HIGH); //todo
+  delay(100);
+  afr = (wbSensor*(wbLo/1023.0))+(wbHi-wbLo);
+  Serial.println(afr); //todo
+  Serial.println(revSensor);
+  Serial.println(spdSensor);
+}
