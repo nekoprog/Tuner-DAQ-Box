666deb68177786f1e2822ee8999654d818b94286
Neko Prog <nekoprog@gmail.com>
2017-04-11 14:03:15 +0800

    Add files via upload

--- TunerDAQBox.ino
+++ TunerDAQBox.ino
@@ -6,7 +6,7 @@ int spdIn = A2;
 int wbSensor = 0;
 unsigned long revSensor = 0; //counts per second
 unsigned long spdSensor = 0; //counts per second
-float afr = 0.0;
+float afr = 0;
 
 void setup() {
 	// put your setup code here, to run once:
@@ -18,12 +18,12 @@ void loop() {
 	wbSensor = analogRead(wbIn);
 	//revSensor = pulseIn(revIn,HIGH); //todo
 	//spdSensor = pulseIn(spdIn,HIGH); //todo
-	afr = (wbLo*(wbSensor/1023))+(wbHi-wbLo); //this shit won't work
-	Serial.print(afr);
+	afr = (wbLo*wbSensor/1024)+(wbHi-wbLo);
+	Serial.print(afr,1);
 	//todo
 	Serial.print(',');
 	Serial.println(0);
 	//Serial.print(',');
 	//Serial.println(spdSensor);
-  delay(50);
+	delay(250);
 }
--- TunerDAQBox.py
+++ TunerDAQBox.py
@@ -9,10 +9,26 @@ plt.ion()
 counter = 0
 
 def liveData():
+    plt.figure(1)
+    plt.subplot(211)
     plt.ylim(9,21)
     plt.xlim(0,50)
     plt.ylabel("AFR")
     plt.plot(afrData)
+
+    '''
+    plt.subplot(212)
+    plt.ylim(10000,-10000)
+    plt.xlim(0,2000)
+    plt.ylabel("RPM")
+    plt.plot() RPM
+
+    plt.subplot(213)
+    plt.ylim(10000,-10000)
+    plt.xlim(0,2000)
+    plt.ylabel("Speed")
+    plt.plot() Speed
+    '''
 
 while True:
     #while(dataStream.inWaiting()==0):
